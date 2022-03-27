506cab6b9041bf34ff128442eac5d18093b4e743
Fiattarone <Fiattarone@me.com>
2022-03-26 22:16:17 -0700

    Finished connecting to github api, wrote mongoose Post Model.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -325,8 +325,22 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
 router.get("/github/:username", (req, res) => {
     try {
         const options = {
-            uri: `https://api.github.com/users/${req.params.username}/repos`
-        }
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&
+            sort=created:asc&client_id=${config.get("githubClientId")}&
+            client_secret=${config.get("githubSecret")}`,
+            method: "GET",
+            headers: { "user-agent": "node.js" } 
+        };
+
+        request(options, (error, response, body) => {
+            if (error) console.error(error);
+
+            if (response.statusCode !== 200) {
+                res.status(404).json({ message: "No Github Profile found." })
+            }
+            
+            res.json(JSON.parse(body))
+        })
     } catch (err) {
         console.error(err.message);
         res.status(500).send("Server Error.");
