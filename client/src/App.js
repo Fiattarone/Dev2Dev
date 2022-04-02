544a0242994d63325a1ed3fc565379a4d6bce64e
Fiattarone <Fiattarone@me.com>
2022-04-01 20:53:42 -0700

    Complete landing, login and register pages, assign react-router v6 routes and establish formdata hook.

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
+const App = () =>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Fragment>
+  </Router>
+  
+
+export default App;
