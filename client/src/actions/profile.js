383f802adb8c1113036e0d202bace17b18cd2c62
Fiattarone <Fiattarone@me.com>
2022-07-07 17:43:27 -0700

    Finish profile actions/reducer

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -4,9 +4,11 @@ import { setAlert } from "./alert";
 import {
     CLEAR_PROFILE,
     GET_PROFILE,
+    GET_PROFILES,
     PROFILE_ERROR,
     UPDATE_PROFILE,
-    ACCOUNT_DELETED
+    ACCOUNT_DELETED,
+    GET_REPOS
 } from "./types";
 
 //get current user's profile
@@ -25,6 +27,60 @@ export const getCurrentProfile = () => async dispatch => {
         });
     }
 }
+
+//get all profiles
+export const getProfiles = () => async dispatch => {
+    dispatch({ type: CLEAR_PROFILE });
+
+    try {
+        const res = await axios.get("/api/profile");
+
+        dispatch({
+            type: GET_PROFILES,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.data.msg, status: err.response.status }
+        });
+    }
+}
+
+//get profile by ID
+export const getProfileById = userId => async dispatch => {
+    try {
+        const res = await axios.get(`/api/profile/user/${userId}`);
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.data.msg, status: err.response.status }
+        });
+    }
+}
+
+//get github repos
+export const getGithubRepos = username => async dispatch => {
+    try {
+        const res = await axios.get(`/api/profile/github/${username}`);
+
+        dispatch({
+            type: GET_REPOS,
+            payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.data.msg, status: err.response.status }
+        });
+    }
+}
+
 
 //Create or update profile
 
