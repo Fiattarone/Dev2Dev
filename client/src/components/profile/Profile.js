a5c84a795d165b274feea04f7b78833ffb7e87d2
Fiattarone <Fiattarone@me.com>
2022-07-09 15:41:08 -0700

    Add profiletop and profileabout components.

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
 import { Link, useParams } from 'react-router-dom';
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
     const { id } = useParams();
@@ -17,6 +19,10 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
             { auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id && (
                 <Link to='/edit-profile'>Edit Profile</Link>
             )}
+        <div class="profile-grid my-1">
+            <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
+        </div>
         </Fragment>)}
     </Fragment>
   )
